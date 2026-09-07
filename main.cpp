e360cb6dbd8fb7b8be8fefd64bd58f602de7246d
junio-moreira <dispmob.ads.iftm@gmail.com>
2026-09-07 17:52:35 -0300

    Update CMakeLists.txt and add main entry with gitignore

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    const auto lang = "C++";
+    cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        cout << "i = " << i << endl;
+    }
+
+    return 0;
+
+}
